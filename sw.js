f54a1a9717cb0ec9b7578ed0d16b1211872b27e7
DaveJamesula <davidjamesula@gmail.com>
2026-07-03 15:49:23 +0800

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,44 @@
+const CACHE_NAME = 'daveai-shell-v1';
+const SHELL_FILES = [
+  './index.html',
+  './manifest.json',
+  './daveai-icon-192.png',
+  './daveai-icon-512.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(SHELL_FILES))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  const url = new URL(event.request.url);
+
+  if (url.hostname === 'api.anthropic.com') {
+    return;
+  }
+
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      if (cached) return cached;
+      return fetch(event.request).then((response) => {
+        if (response && response.ok && event.request.method === 'GET') {
+          const clone = response.clone();
+          caches.open(CACHE_NAME).then((cache) => cache.put(event.request, clone));
+        }
+        return response;
+      }).catch(() => cached);
+    })
+  );
+});
